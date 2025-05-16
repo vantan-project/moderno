1b543886870e687e3cbeacaf8d49d3416c9ead7a
hinata yokoyama <hinata@example.com>
2025-05-16 16:04:13 +0900

    オーダーカテゴリー追加

---
+++ back/database/migrations/2025_05_16_065315_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBicInteger('user_id');
+            $table->unsignedBicInteger('furniture_id');
+            $table->unsignedInteger('count');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('furniture_id')
+                ->references('id')
+                ->on('furniture')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
