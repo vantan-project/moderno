3c1c74dc42b7f06c64ef428f3320f8630935ca6c
hinata yokoyama <hinata@example.com>
2025-05-16 15:25:49 +0900

    家具カテゴリー

---
+++ back/database/migrations/2025_05_16_060711_create_furniture_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('furniture', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('image_url');
+            $table->text('detail');
+            $table->decimal('price');
+            $table->id('category_id');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('furniture');
+    }
+};
