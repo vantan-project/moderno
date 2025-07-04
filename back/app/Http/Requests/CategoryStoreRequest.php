d8a4df889c2607e63ddf0b9335dcc16f3bc5720c
hinata yokoyama <hinata@example.com>
2025-07-04 15:00:50 +0900

    変更

---
+++ back/app/Http/Requests/CategoryStoreRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CategoryStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category.name' => 'required|string|max:255|regex:/[a-zA-Zぁ-んァ-ヶ一-龠]/',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return[
+            'category.name.required' => 'カテゴリー名は必須です。',
+            'category.name.string' => 'カテゴリー名は文字列で入力してください。',
+            'category.name.max' => 'カテゴリー名は255文字以内で入力してください。',
+            'category.name.regex' => 'カテゴリー名は文字列で入力してください。',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success' => false,
+                'messages' => collect($validator->errors()->messages())
+                    ->flatten()
+                    ->toArray()
+            ], 422)
+        );
+    }
+}
