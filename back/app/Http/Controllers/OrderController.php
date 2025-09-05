1ffa8b34df2d124d703da31e7dcc76afe68778c8
hinata yokoyama <hinata@example.com>
2025-09-05 14:31:23 +0900

    変更

--- back/app/Http/Controllers/OrderController.php
+++ back/app/Http/Controllers/OrderController.php
@@ -61,9 +61,9 @@ class OrderController extends Controller
     }
 
     public function history(Request $request) {
-        $authUser = request()->user();
         $currentPage = $request['currentPage'];
-        $orders = $authUser->orders()->with('furniture');
+        $orders = Order::with('furniture')
+            ->where('is_shipped',true);
         $PER_PAGE = 20;
         $orders = $orders->paginate($PER_PAGE, ['*'], 'page', $currentPage);
 
