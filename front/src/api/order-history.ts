a6018b091aa67137a846115ab9e47c381d490898
Kate0418 <nakao.keito0418@gmail.com>
2025-10-10 14:48:34 +0900

    historyページ用のAPI定義

---
+++ front/src/api/order-history.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export type OrderHistoryRequest = {
+  currentPage: number;
+};
+
+export type OrderHistoryResponse = {
+  success: boolean;
+  orders: {
+    id: number;
+    furniture: {
+      id: number;
+      name: string;
+      imageUrl: string;
+    };
+    count: number;
+    isShipped: boolean;
+    isCompleted: boolean;
+  }[];
+  lastPage: number;
+};
+
+export function orderHistory({
+  currentPage,
+}: OrderHistoryRequest): Promise<OrderHistoryResponse> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/order/History`;
+  const authToken = Cookies.get("authToken");
+
+  return axios
+    .get<OrderHistoryResponse>(apiUrl, {
+      params: { currentPage },
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      console.log(err);
+      return {
+        success: false,
+        orders: [],
+        lastPage: 0,
+      };
+    });
+}
