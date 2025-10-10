5213d39a5b779efbcc23a83aee9846ad55111f21
hinata yokoyama <hinata@example.com>
2025-10-10 14:23:05 +0900

    履歴アイコン

--- front/src/components/layout/items/user-controls.tsx
+++ front/src/components/layout/items/user-controls.tsx
@@ -2,6 +2,7 @@ import { token } from "@/api/token";
 import { ButtonWithLabel } from "@/components/shared/button-with-label";
 import { CartIcon } from "@/components/shared/icons/cart-icon";
 import { HeartIcon } from "@/components/shared/icons/heart-icon";
+import { HistoryIcon } from "@/components/shared/icons/history-icon";
 import { LoginIcon } from "@/components/shared/icons/login-icon";
 import { UserIcon } from "@/components/shared/icons/user-icon";
 import { useGlobalContext } from "@/hooks/use-global-state";
@@ -20,9 +21,21 @@ export function UserControls() {
 
     router.push("/like");
   };
+  const handleHistory = async () => {
+    const tokenRes = await token();
+    if (!tokenRes.success) {
+      router.push("/login");
+      return;
+    }
+
+    router.push("/history");
+  };
 
   return (
     <div className="flex items-center gap-2 bg-[rgba(255,255,255,0.7)] backdrop-filter-[blur(8px)] text-void rounded-2xl py-2 px-5 [&>*]:w-16">
+      <ButtonWithLabel onClick={handleHistory} label="履歴">
+        <HistoryIcon className={iconClassName} />
+      </ButtonWithLabel>
       <ButtonWithLabel onClick={handleLike} label="お気に入り">
         <HeartIcon className={iconClassName} />
       </ButtonWithLabel>
